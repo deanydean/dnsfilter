dc5dca4a4247564cd3a281225659298888c9701a
Deany Dean <deanydean@shells>
2016-03-20 20:09:56 +0000

    Added cli handling for port and whitelist options

--- src/dns-server.py
+++ src/dns-server.py
@@ -17,18 +17,19 @@
 #
 # A DNS server that will only resolve named from a set of white-listed domains
 #
+import argparse
 from twisted.internet import reactor
 from twisted.names import client, dns, server
 from wlresolver import WhitelistResolver
 
-whitelistFile = "etc/domain-whitelist.conf"
+DEFAULT_WHITELISTFILE = "etc/domain-whitelist.conf"
 
-def start_dns_server():
+def start_dns_server(args):
     """
     Run the DNS server.
     """
     defResolver = client.Resolver(resolv='/etc/resolv.conf')
-    wlResolver = WhitelistResolver(defResolver, whitelistFile)
+    wlResolver = WhitelistResolver(defResolver, args.whitelist)
 
     factory = server.DNSServerFactory(
         clients=[wlResolver]
@@ -36,10 +37,18 @@ def start_dns_server():
     
     protocol = dns.DNSDatagramProtocol(controller=factory)
 
-    reactor.listenUDP(10053, protocol)
-    reactor.listenTCP(10053, factory)
+    reactor.listenUDP(args.port, protocol)
+    reactor.listenTCP(args.port, factory)
 
     reactor.run()
 
+# Read options from CLI
+parser = argparse.ArgumentParser(description="Start the DNS server")
+parser.add_argument('--port', nargs='?', type=int, default=53,
+    help="Port to listen on")
+parser.add_argument('--whitelist', nargs='?',
+    default=DEFAULT_WHITELISTFILE, help="The whitelist file")
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    raise SystemExit(start_dns_server())
+    raise SystemExit(start_dns_server(args))
